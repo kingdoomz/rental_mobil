dcf8b5a8ddb2418b1e191c7775c8f1071ac753a2
kingdoomz <arp8395@gmail.com>
2021-09-20 22:08:48 +0700

    upload project

---
+++ application/controllers/Contoh01.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh01 extends CI_Controller {
+
+	public function index()
+	{
+		echo"<h1>Halo</h1>";
+		echo"</br>Nama saya Ridwan
+		</br>Saya tinggal di daerah Ciputat
+		</br>olah raga yang saya sukai adalah
+		</br>Bulutangkis";
+	}
+}
